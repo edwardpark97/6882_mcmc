c33a6df98f3ad7258ca420c9a4f7a1c937afd1aa
Edward Park <edwardpark97@gmail.com>
2018-05-11 07:19:58 +0000

    finalized all code, ready for 20k data run

--- mh_base.py
+++ mh_base.py
@@ -7,8 +7,6 @@ import process_freddie
 import os
 import time
 
-PARALLEL = 1
-
 class MCMCSampler(object):
 	def __init__(self, log_f, log_g, g_sample, x0, iterations):
 		"""
@@ -26,14 +24,18 @@ class MCMCSampler(object):
 		self.start_state = x0
 		self.state = x0
 		self.iterations = iterations
-
 		self.saved_states = [x0]
 
 	def sample(self):
 		raise NotImplementedError
 
-	def calculate_acceptance_ratio(self, proposal_state):
-		raise NotImplementedError
+	def exponentiate_min1(self, log):
+		if log > 0:
+			return 1
+		elif log < -20:
+			return 0
+		else:
+			return np.exp(log)
 
 	def transition_step(self, cur_state, candidate_state, acceptance_ratio):
 		u = np.random.uniform()
@@ -42,28 +44,25 @@ class MCMCSampler(object):
 	def get_saved_states(self):
 		return self.saved_states
 
+
 class MHSampler(MCMCSampler):
+	# take log_f as a parameter to make it more robust for consensus as well
+	def calculate_acceptance_ratio(self, proposal_state, log_f):
+		log = log_f(proposal_state) + self.log_transition_probabilities(self.state, proposal_state) - log_f(self.state) - self.log_transition_probabilities(proposal_state, self.state)
+		return self.exponentiate_min1(log)
+
 	def sample(self):
 		for i in range(self.iterations):
-			if i % 500 == 0:
+			if i % 5000 == 0:
 				print("iteration {}".format(i))
 			candidate_state = self.get_transition_sample(self.state)
-			acceptance = self.calculate_acceptance_ratio(candidate_state)
+			acceptance = self.calculate_acceptance_ratio(candidate_state, self.log_distribution_fn)
 			new_state = self.transition_step(self.state, candidate_state, acceptance)
 			self.saved_states.append(new_state)
 			self.state = new_state
 
-	def calculate_acceptance_ratio(self, proposal_state):
-		log = self.log_distribution_fn(proposal_state) + self.log_transition_probabilities(self.state, proposal_state) - self.log_distribution_fn(self.state) - self.log_transition_probabilities(proposal_state, self.state)
-		if log > 0:
-			acceptance_ratio = 1
-		elif log < -20:
-			acceptance_ratio = 0
-		else:
-			acceptance_ratio = np.exp(log)
-		return min(1, acceptance_ratio)
 
-class ConsensusMHSampler(MCMCSampler):
+class ConsensusMHSampler(MHSampler):
 	def __init__(self, log_f, log_g, g_sample, x0, iterations, shards=1):
 		super(ConsensusMHSampler, self).__init__(log_f, log_g, g_sample, x0, iterations)
 		self.shards = shards
@@ -79,23 +78,35 @@ class ConsensusMHSampler(MCMCSampler):
 		map_results = self.pool.map(self.map_sample, range(self.shards))
 		self.pool.close()
 		self.pool.join()
+		self.pool.terminate()
+		self.pool.restart()
 		self.saved_states = self.reduce_sample(map_results)
 
 	def map_sample(self, index):
 		np.random.seed(1)
 		cur_state = self.start_state
 		sample_results = [cur_state]
+		prob, count = 0, 0
+
 		for i in range(self.iterations):
-			if i % 500 == 0:
+			if i % 5000 == 0:
 				print("iteration {}".format(i))
 			candidate_state = self.get_transition_sample(cur_state)
-			acceptance = self.calculate_acceptance_ratio(candidate_state, index)
+			acceptance = self.calculate_acceptance_ratio(candidate_state, self.log_fn_dict[index])
+			prob += acceptance
+			count += 1
+
 			new_state = self.transition_step(cur_state, candidate_state, acceptance)
 			sample_results.append(new_state)
 			cur_state = new_state
 		sample_results = np.array(sample_results)
 
-		return (sample_results, 1.0 / (1e-8 + get_sample_variance(sample_results)))
+		print("INDEX {}: Avg acceptance prob is {}".format(index, prob/count))
+
+		return (sample_results, 1.0 / (1e-8 + self.get_sample_variance(sample_results)))
+
+	def get_sample_variance(self, data):
+		return np.linalg.norm(np.var(np.array(data), axis=0))
 
 	def reduce_sample(self, results):
 		'''
@@ -109,22 +120,10 @@ class ConsensusMHSampler(MCMCSampler):
 
 		return sample_results / total_weight
 
-	def calculate_acceptance_ratio(self, proposal_state, index=0):
-		log_fn = self.log_fn_dict[index]
-		log = log_fn(proposal_state) + self.log_transition_probabilities(self.state, proposal_state) - log_fn(self.state) - self.log_transition_probabilities(proposal_state, self.state)
-		if log > 0:
-			acceptance_ratio = 1
-		elif log < -20:
-			acceptance_ratio = 0
-		else:
-			acceptance_ratio = np.exp(log)
-		# print("%s %s" % (index, min(1, acceptance_ratio)))
-		return min(1, acceptance_ratio)
 
 class TwoStageMHSampler(MHSampler):
 	def __init__(self, log_f1, log_f2, log_g, g_sample, x0, iterations):
 		super(TwoStageMHSampler, self).__init__(log_f1, log_g, g_sample, x0, iterations)
-
 		self.log_f1 = log_f1
 		self.log_f2 = log_f2
 		self.stage_1_prob, self.stage_1_count = 0, 0
@@ -134,17 +133,21 @@ class TwoStageMHSampler(MHSampler):
 	def sample(self):
 		for i in range(self.iterations):
 			start_time = time.time()
-			if i % 500 == 0:
+			if i % 5000 == 0:
 				print("iteration {}".format(i))
 			candidate_state = self.get_transition_sample(self.state)
-			log_acceptance_ratio1 = self.calculate_log_acceptance_ratio1(candidate_state) # store for accep_ratio2 calculation
-			acceptance1 = self.calculate_acceptance_ratio1(log_acceptance_ratio1)
+			log_acceptance_ratio1 = self.calculate_log_acceptance_ratio1(candidate_state, self.log_f1) # store for accep_ratio2 calculation
+			acceptance1 = self.exponentiate_min1(log_acceptance_ratio1)
+			self.stage_1_count += 1
+			self.stage_1_prob += acceptance1
 			time1 = time.time() - start_time
 
 			if np.random.uniform() < acceptance1: # do second stage
-				acceptance2 = self.calculate_acceptance_ratio2(candidate_state, log_acceptance_ratio1)
+				acceptance2 = self.calculate_acceptance_ratio2(candidate_state, log_acceptance_ratio1, self.log_f2)
+				self.stage_2_count += 1
+				self.stage_2_prob += acceptance2
+
 				if np.random.uniform() < acceptance2:
-					# print("moved")
 					self.state = candidate_state
 				self.ratio_time += (time.time() - start_time) / time1
 				self.ratio_time_count += 1
@@ -154,34 +157,67 @@ class TwoStageMHSampler(MHSampler):
 		print("Avg speedup per stage 2 skipped is {}".format(self.ratio_time / self.ratio_time_count))  
 		print("Avg stage 1 prob is {}, avg stage 2 prob is {}".format(self.stage_1_prob/self.stage_1_count, self.stage_2_prob/self.stage_2_count))
 
-	def calculate_log_acceptance_ratio1(self, proposal_state):
-		return self.log_f1(proposal_state) + self.log_transition_probabilities(self.state, proposal_state) - self.log_f1(self.state) - self.log_transition_probabilities(proposal_state, self.state)
+	# more robust for consensus
+	def calculate_log_acceptance_ratio1(self, proposal_state, log_f1):
+		return log_f1(proposal_state) + self.log_transition_probabilities(self.state, proposal_state) - log_f1(self.state) - self.log_transition_probabilities(proposal_state, self.state)
 
-	def calculate_acceptance_ratio1(self, log):
-		if log > 0:
-			acceptance_ratio = 1
-		elif log < -20:
-			acceptance_ratio = 0
-		else:
-			acceptance_ratio = np.exp(log)
-		self.stage_1_count += 1
-		self.stage_1_prob += min(1, acceptance_ratio)
-		return min(1, acceptance_ratio)
+	# again, take log_f2 as a parameter
+	def calculate_acceptance_ratio2(self, proposal_state, log_accept_ratio1, log_f2):
+		log = log_f2(proposal_state) - log_f2(self.state) - log_accept_ratio1
+		return self.exponentiate_min1(log)
 
-	def calculate_acceptance_ratio2(self, proposal_state, log_accept_ratio1):
-		log = self.log_f2(proposal_state) - self.log_f2(self.state) - log_accept_ratio1
-		if log > 0:
-			acceptance_ratio = 1
-		elif log < -20:
-			acceptance_ratio = 0
-		else:
-			acceptance_ratio = np.exp(log)
-		self.stage_2_count += 1
-		self.stage_2_prob += min(1, acceptance_ratio)
-		return min(1, acceptance_ratio)
 
-def get_sample_variance(data):
-	return np.linalg.norm(np.var(np.array(data), axis=0))
+class TwoStageConsensusMHSampler(ConsensusMHSampler):
+	def __init__(self, log_f1, log_f2, log_g, g_sample, x0, iterations, shards=1):
+		super(TwoStageConsensusMHSampler, self).__init__(log_f1, log_g, g_sample, x0, iterations, shards=shards)
+		assert len(log_f2) == self.shards
+		self.log_fn_dict2 = {} # for pickling purposes
+		for i in range(self.shards):
+			self.log_fn_dict2[i] = log_f2[i]
+
+	def map_sample(self, index):
+		np.random.seed(1)
+		cur_state = self.start_state
+		sample_results = [cur_state]
+		stage_1_prob, stage_1_count, stage_2_prob, stage_2_count, ratio_time, ratio_time_count = 0, 0, 0, 0, 0, 0
+
+		for i in range(self.iterations):
+			start_time = time.time()
+			if i % 5000 == 0:
+				print("iteration {}".format(i))
+			candidate_state = self.get_transition_sample(cur_state)
+			log_acceptance_ratio1 = self.calculate_log_acceptance_ratio1(candidate_state, self.log_fn_dict[index])
+			acceptance1 = self.exponentiate_min1(log_acceptance_ratio1)
+
+			stage_1_count += 1
+			stage_1_prob += acceptance1
+			time1 = time.time() - start_time
+
+			if np.random.uniform() < acceptance1: # do second stage
+				acceptance2 = self.calculate_acceptance_ratio2(candidate_state, log_acceptance_ratio1, self.log_fn_dict2[index])
+				stage_2_count += 1
+				stage_2_prob += acceptance2
+
+				if np.random.uniform() < acceptance2:
+					cur_state = candidate_state
+				ratio_time += (time.time() - start_time) / time1
+				ratio_time_count += 1
+
+			sample_results.append(cur_state)
+
+		print("INDEX {}: Avg speedup per stage 2 skipped is {}".format(index, ratio_time / ratio_time_count))  
+		print("INDEX {}: Avg stage 1 prob is {}, avg stage 2 prob is {}".format(index, stage_1_prob/stage_1_count, stage_2_prob/stage_2_count))
+		sample_results = np.array(sample_results)
+		return (sample_results, 1.0 / (1e-8 + self.get_sample_variance(sample_results)))
+
+	# i think there's a way to inherit these two, but it's more trouble than it's worth
+	def calculate_log_acceptance_ratio1(self, proposal_state, log_f1):
+		return log_f1(proposal_state) + self.log_transition_probabilities(self.state, proposal_state) - log_f1(self.state) - self.log_transition_probabilities(proposal_state, self.state)
+	
+	def calculate_acceptance_ratio2(self, proposal_state, log_accept_ratio1, log_f2):
+		log = log_f2(proposal_state) - log_f2(self.state) - log_accept_ratio1
+		return self.exponentiate_min1(log)
+
 
 def calculate_ess(samples): # uses matplotlib to calculate ESS for each dimension, return min
 	num_samples, num_features = samples.shape[0], samples.shape[1]
@@ -198,7 +234,7 @@ def calculate_edpm(ess, seconds):
 	return ess * 60. / seconds
 
 # using main MH on the bank data
-def generate_samples(dataset, sampling_method, a=400000):
+def generate_samples(dataset, sampling_method, num_subsamples=200000):
 	np.random.seed(1)
 	if dataset == "bank_small":
 		# N=100000 takes 1-2 min
@@ -212,19 +248,21 @@ def generate_samples(dataset, sampling_method, a=400000):
 		x0 = [-2.45, .02, -.125, -.35, -.14, -.75, .35, .1, .1, -.45]
 	elif dataset == "freddie_mac":
 		# N=2000 takes about 7 min, N=100000 takes ~6 hours
-		N = 5000
+		N = 20000
 		if sampling_method == "MH":
 			proposal_variance = 4e-5
-		elif sampling_method == "consensus":
-			proposal_variance = 8e-5
+		elif sampling_method == "Consensus":
+			proposal_variance = 2e-4
 		elif sampling_method == "TwoStage":
 			proposal_variance = 8e-6
+		elif sampling_method == "TwoStageConsensus":
+			proposal_variance = 5e-5
 		feature_array, output_vector = process_freddie.create_arrays()
 		x0 = [-6.25, -.72, -.23, .56, .04, .13, .05, -.06, 0, .30]
 	else:
 		assert False
 
-	print("\nUsing dataset {}, sampling_method {}, a={}, N={}, proposal_variance={}".format(dataset, sampling_method, a, N, proposal_variance))
+	print("\nUsing dataset {}, sampling_method {}, num_subsamples={}, N={}, proposal_variance={}".format(dataset, sampling_method, num_subsamples, N, proposal_variance))
 	burnin = int(.4 * N)
 	num_points, num_features = feature_array.shape[0], feature_array.shape[1]
 	prior_mean, prior_variance = np.zeros(num_features), 100
@@ -246,7 +284,24 @@ def generate_samples(dataset, sampling_method, a=400000):
 		prior = log_multivariate_gaussian_pdf(prior_mean, val, prior_variance)
 		return np.sum(p_data) + prior / shards # actually is a log value
 
-	if sampling_method == "consensus":
+	# generates the stage 1 functions used in twostage
+	def generate_stage_1_f(features, outputs, shards=1):
+		true_indices, false_indices = np.nonzero(outputs)[0], np.nonzero(1 - outputs)[0]
+		a = num_subsamples # number of false entries to subsample for the first stage (out of ~2million)
+		num_false_entries = len(false_indices)
+		false_indices = np.random.choice(false_indices, a, replace=False)
+		true_feature_array, false_feature_array = features[true_indices], features[false_indices]
+		def log_f1(val):
+			# does calculations separately between true + false subsampled data points
+			theta = true_feature_array.dot(val)
+			true_contribution = np.sum(theta - np.log(1 + np.exp(theta)))
+			theta = false_feature_array.dot(val)
+			false_contribution = num_false_entries / a * np.sum(- np.log(1 + np.exp(theta)))
+			prior = log_multivariate_gaussian_pdf(prior_mean, val, prior_variance)
+			return true_contribution + false_contribution + prior / shards
+		return log_f1
+
+	if "Consensus" in sampling_method:
 		p = np.random.permutation(num_points)
 		feature_array, output_vector = feature_array[p], output_vector[p]
 		split_indices = []
@@ -260,44 +315,36 @@ def generate_samples(dataset, sampling_method, a=400000):
 				lambda val: f(val, split_feature_array[2], split_output_vector[2], 4),
 				lambda val: f(val, split_feature_array[3], split_output_vector[3], 4)]
 
-		start_time = time.time()
-		sampler = ConsensusMHSampler(log_fns, log_g_transition_prob, g_sample, x0, N, shards=4)
+		if sampling_method != "TwoStageConsensus":
+			sampler = ConsensusMHSampler(log_fns, log_g_transition_prob, g_sample, x0, N, shards=4)
+		else:
+			# do the two stage part of it too
+			log_fns1 = [generate_stage_1_f(split_feature_array[0], split_output_vector[0], shards=4),
+						generate_stage_1_f(split_feature_array[1], split_output_vector[1], shards=4),
+						generate_stage_1_f(split_feature_array[2], split_output_vector[2], shards=4),
+						generate_stage_1_f(split_feature_array[3], split_output_vector[3], shards=4)]
+			sampler = TwoStageConsensusMHSampler(log_fns1, log_fns, log_g_transition_prob, g_sample, x0, N, shards=4)
 	
 	elif sampling_method == "MH":
 		log_f_distribution_fn = lambda val: f(val, feature_array, output_vector)
-		start_time = time.time()
 		sampler = MHSampler(log_f_distribution_fn, log_g_transition_prob, g_sample, x0, N)
 	
 	elif sampling_method == "TwoStage":
 		log_f2 = lambda val: f(val, feature_array, output_vector)
-		true_indices, false_indices = np.nonzero(output_vector)[0], np.nonzero(1 - output_vector)[0]
-
-		# a = 400000 # number of false entries to subsample for the first stage (out of ~2million)
-		num_false_entries = len(false_indices)
-		false_indices = np.random.choice(false_indices, a, replace=False)
-		true_feature_array, false_feature_array = feature_array[true_indices], feature_array[false_indices]
-
-		def log_f1(val):
-			# does calculations separately between true + false subsampled data points
-			theta = true_feature_array.dot(val)
-			true_contribution = np.sum(theta - np.log(1 + np.exp(theta)))
-			theta = false_feature_array.dot(val)
-			false_contribution = num_false_entries / a * np.sum(- np.log(1 + np.exp(theta)))
-			prior = log_multivariate_gaussian_pdf(prior_mean, val, prior_variance)
-			return true_contribution + false_contribution + prior
-		start_time = time.time()
+		log_f1 = generate_stage_1_f(feature_array, output_vector)
 		sampler = TwoStageMHSampler(log_f1, log_f2, log_g_transition_prob, g_sample, x0, N)
 	else:
 		assert False
 
+	start_time = time.time()
 	sampler.sample()
 	runtime = time.time() - start_time
 	print("{} run time: {}\n".format(sampling_method, runtime))
 
 	samples = sampler.get_saved_states()
-	samples = np.array(samples[burnin:])
+	np.save("samples/{}_0_{}_{}.npy".format(sampling_method, N, int(runtime)), samples)
 
-	np.save("samples/{}_{}_{}_{}_{}.npy".format(sampling_method, burnin, N, int(runtime), a), samples)
+	samples = np.array(samples[burnin:])
 	return samples, runtime
 
 def plot_tracking(samples, index, directory_path, sampling_method):
@@ -324,14 +371,14 @@ def plot_tracking(samples, index, directory_path, sampling_method):
 	plt.clf()
 
 # a is the # of datapoints chosen for the first stage of twostage
-def main(dataset, sampling_method, sample_path="", a=400000):
+def main(dataset, sampling_method, sample_path="", num_subsamples=200000):
 	if os.path.isfile(sample_path):
 		samples = np.load(sample_path)
 
 		file_name = sample_path.split("/")[-1].split(".")[0]
 		runtime = float(file_name.split("_")[3]) # assuming same naming conventions samplingMethod_burnin_N_runtime.npy
 	else:
-		samples, runtime = generate_samples(dataset, sampling_method, a=a)
+		samples, runtime = generate_samples(dataset, sampling_method, num_subsamples=num_subsamples)
 
 	for i in range(num_features):
 	  plot_tracking(samples, i, "plots/{}".format(dataset), sampling_method)
@@ -342,6 +389,7 @@ def main(dataset, sampling_method, sample_path="", a=400000):
 	print("EDPM: {}\n".format(effective_draws_per_min))
 
 if __name__ == '__main__':
-	# main("freddie_mac", "TwoStage", sample_path="samples/consensus_4000_10000_2736.npy")
-	# main("freddie_mac", "TwoStage", sample_path="samples/MH_4000_10000_4262.npy")
-	# main("freddie_mac", "TwoStage", sample_path="samples/TwoStage_4000_10000_2839.npy")
+	main("freddie_mac", "TwoStageConsensus")
+	main("freddie_mac", "TwoStage")
+	main("freddie_mac", "Consensus")
+	main("freddie_mac", "MH")
